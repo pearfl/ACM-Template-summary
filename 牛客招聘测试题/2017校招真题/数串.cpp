47c16b433e85c217c60b03ddeb941f837ae16118
PearFL <451159561@qq.com>
2018-03-15 20:38:55 +0800

    牛客练习题

---
+++ 牛客招聘测试题/2017校招真题/数串.cpp
@@ -0,0 +1,36 @@
+/*
+数串
+*/
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+ 
+using namespace std;
+ 
+int main(){
+    int count;
+    while(cin>>count){
+        vector<string> inputStrs;
+        string curInputStr;
+        for(int i = 0; i<count; ++i){
+            cin>>curInputStr;
+            inputStrs.push_back(curInputStr);
+        }
+             
+        for(int shift = 0; shift<inputStrs.size()-1; ++shift){
+            for(int index = 0; index<inputStrs.size()-1-shift; ++index){
+                if((inputStrs[index]+inputStrs[index+1])<(inputStrs[index+1]+inputStrs[index])){
+                swap(inputStrs[index],inputStrs[index+1]);
+                }
+            }
+        }
+     
+        for(string str: inputStrs){
+            cout<<str;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
