bab9d6d6c740770327b4dea7cb5a4a1e921e33dc
PearFL <451159561@qq.com>
2018-04-10 15:12:29 +0800

    调整了文件位置

---
+++ temp/2.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#define pi 3.1415926
+
+int main(){
+    //freopen("input.txt","r",stdin);
+    double r = 2.5,h = 1.8;
+    double s = pi*r*r;
+    double v = pi*r*h;
+
+    printf("%lf %lf",s,v);
+
+    return 0;
+}
